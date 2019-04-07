31cc4fcaab477a74f49e9b8e87cbfeef9d96c936
Jan Nowaczek <jasiek19_85@o2.pl>
2019-04-07 16:47:57 +0200

    inc +5 and dec -5 added to Counter

---
+++ src/Counter.js
@@ -0,0 +1,67 @@
+import React, { Component } from 'react';
+
+class Counter extends Component {
+    state = {
+        number: this.props.number
+    }
+
+    inc1(){
+        this.setState({ number: this.state.number + 1 })
+    }
+
+    dec1(){
+        this.setState({ number: this.state.number - 1 })
+    }
+
+    inc5(){
+        this.setState({ number: this.state.number + 5 })
+    }
+
+    dec5(){
+        this.setState({ number: this.state.number - 5 })
+    }
+
+    reset(){
+        this.setState({number: this.props.number})
+    }
+
+
+
+    render() {
+        return (
+            <div>
+                <h1>
+                    {this.state.number}
+                </h1>
+
+                <button
+                    onClick={this.inc1}
+                >
+                    + 1
+                </button>
+                <button
+                    onClick={this.dec1}
+                >
+                    - 1
+                </button>
+                <button
+                    onClick={this.inc5}
+                >
+                    + 5
+                </button>
+                <button
+                    onClick={this.dec5}
+                >
+                    - 5
+                </button>
+                <button
+                    onClick={() => this.reset}
+                >
+                    Reset
+                </button>
+            </div>
+        )
+    }
+}
+
+export default Counter
